537839977d5b0e6afa2b88fb4501a3af6772c729
Jessica Gladstone <jcg321@comcast.net>
2019-02-02 19:32:30 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,4 +3,8 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
   
   actor = Actor.create(first_name: "Emilia", last_name: "Clark")
+  
+  def full_name
+    full_name = #{first_name} + #{last_name}
+  end
 end

dab03bafd7c171015532ef01d32f4621c90488f2
Jessica Gladstone <jcg321@comcast.net>
2019-02-02 20:30:53 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,7 @@ class Actor < ActiveRecord::Base
   end
   
   def list_roles
-    self.shows.collect {|character|character.shows.name}
+    self.shows.collect {|character|character.name}
       
   end
 end
